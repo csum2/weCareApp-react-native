7f6745e276d5d81fc5958fc24e4c49bb37bf62a1
Samuel Sum <sch.developer@gmail.com>
2021-12-06 18:46:57 -0500

    coded delete a patient

--- navigators/TabViewPatient.js
+++ navigators/TabViewPatient.js
@@ -5,13 +5,18 @@
  *
  */
 import React from 'react';
-import {Alert} from "react-native";
+import {Alert} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import { MaterialHeaderButtons } from './../components/WecareHeaderButton';
-import { Item, HiddenItem, OverflowMenu, Divider } from 'react-navigation-header-buttons';
+import {MaterialHeaderButtons} from './../components/WecareHeaderButton';
+import {
+  Item,
+  HiddenItem,
+  OverflowMenu,
+  Divider,
+} from 'react-navigation-header-buttons';
 
 // Import Screens
 import ViewPatientBasicScreen from './../Screens/ViewPatientBasicScreen';
@@ -25,7 +30,7 @@ const {logoColor, backgroundApp} = Colors;
 
 //Android emulator use HOST = 'http://10.0.2.2:5000'
 //IOS simulator use HOST = 'http://127.0.0.1:5000'
-const HOST = 'https://rest-wecare.herokuapp.com'
+const HOST = 'https://rest-wecare.herokuapp.com';
 
 const Tab = createBottomTabNavigator();
 
@@ -33,38 +38,46 @@ const Tab = createBottomTabNavigator();
 const focusedTab = {active: ''};
 export const FocusedTabContext = React.createContext(null);
 
-const TabViewPatient = ({ route, navigation }) => {
-
+const TabViewPatient = ({route, navigation}) => {
   const handleEditModePress = () => {
     //navigation.navigate('TabEditPatient', route.params);
-    console.log("TabViewPatient, handleEditModePress, focusedTab:" + focusedTab.active );
-    if (focusedTab.active == "PatientMedicalScreen") {
+    console.log(
+      'TabViewPatient, handleEditModePress, focusedTab:' + focusedTab.active,
+    );
+    if (focusedTab.active == 'PatientMedicalScreen') {
       // trigger the save function in the Patient Medical Screen
-      console.log("TabViewPatient, edit mode submit medical data" );
-      route.params.mode = "edit";
+      console.log('TabViewPatient, edit mode submit medical data');
+      route.params.mode = 'edit';
       navigation.navigate('AddEditPatientMedicalScreen', route.params);
     } else {
       // trigger the save function in the Patient Basic Screen
-      console.log("TabViewPatient, edit mode submit basic info" );
-      route.params.mode = "edit";
+      console.log('TabViewPatient, edit mode submit basic info');
+      route.params.mode = 'edit';
       navigation.navigate('AddEditPatientBasicScreen', route.params);
     }
   };
 
   const handleDeletePress = () => {
-    console.log("TabViewPatient, handleDeletePress, focusedTab:" + focusedTab.active );
-    if (focusedTab.active == "PatientMedicalScreen") {
-      console.log("TabViewPatient, delete medical data" );
-      if (route.params.medicaldata !== undefined && route.params.medicaldata.length) {
+    console.log(
+      'TabViewPatient, handleDeletePress, focusedTab:' + focusedTab.active,
+    );
+    if (focusedTab.active == 'PatientMedicalScreen') {
+      console.log('TabViewPatient, delete medical data');
+      if (
+        route.params.medicaldata !== undefined &&
+        route.params.medicaldata.length
+      ) {
         deleteMedical(route.params, navigation);
       } else {
-        Alert.alert(
-          "No Medical Data", "nothing to delete!",
-          [ { text: "OK" } ]
-        );
+        Alert.alert('No Medical Data', 'nothing to delete!', [{text: 'OK'}]);
       }
-    } else {
-      console.log("TabViewPatient, delete basic info" );
+    }
+    // else {
+    //   console.log('TabViewPatient, delete basic info');
+    // }
+    if (focusedTab.active == 'PatientBasicScreen') {
+      console.log('TabViewPatient, delete Patient');
+      deletePatient(route.params, navigation);
     }
   };
 
@@ -74,13 +87,14 @@ const TabViewPatient = ({ route, navigation }) => {
       headerRight: () => (
         <MaterialHeaderButtons>
           <OverflowMenu
-            style={{ marginHorizontal: 10 }}
-            OverflowIcon={({ color }) => (
-              <MaterialIcons name={Platform.OS === 'ios' ? "more-horiz" : "more-vert"}
-                size={30} color={backgroundApp} />
-
-            )}
-          >
+            style={{marginHorizontal: 10}}
+            OverflowIcon={({color}) => (
+              <MaterialIcons
+                name={Platform.OS === 'ios' ? 'more-horiz' : 'more-vert'}
+                size={30}
+                color={backgroundApp}
+              />
+            )}>
             <HiddenItem title="Edit Mode" onPress={handleEditModePress} />
             <HiddenItem title="Delete Record" onPress={handleDeletePress} />
           </OverflowMenu>
@@ -91,73 +105,101 @@ const TabViewPatient = ({ route, navigation }) => {
 
   return (
     <FocusedTabContext.Provider value={focusedTab}>
-    <Tab.Navigator initialRouteName="ViewPatientBasicScreen"
-    screenOptions={{
-      tabBarActiveTintColor: `${logoColor}`,
-    }}
-    >
-      <Tab.Screen
-        name="ViewPatientBasicScreen"
-        component={ViewPatientBasicScreen}
-        initialParams={route.params}
-         options={{
-          headerShown: false,
-          tabBarLabel: 'Basic Information',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="folder-information" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="ViewPatientMedicalScreen"
-        component={ViewPatientMedicalScreen}
-        initialParams={route.params}
-         options={{
-          headerShown: false,
-          tabBarLabel: 'Medical Information',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="medical-bag" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="ViewPatientBasicScreen"
+        screenOptions={{
+          tabBarActiveTintColor: `${logoColor}`,
+        }}>
+        <Tab.Screen
+          name="ViewPatientBasicScreen"
+          component={ViewPatientBasicScreen}
+          initialParams={route.params}
+          options={{
+            headerShown: false,
+            tabBarLabel: 'Basic Information',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons
+                name="folder-information"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="ViewPatientMedicalScreen"
+          component={ViewPatientMedicalScreen}
+          initialParams={route.params}
+          options={{
+            headerShown: false,
+            tabBarLabel: 'Medical Information',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons
+                name="medical-bag"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </FocusedTabContext.Provider>
   );
 };
 
 async function deleteMedical(paramPatient, navigation) {
-  const recentData = getLatestData(paramPatient.medicaldata, "sortkey");
-  console.log("TabViewPatient, deleteMedical, recentData:" + JSON.stringify(recentData));
-  uri = HOST + '/patients/' + paramPatient._id + '/medical/' + recentData._id
+  const recentData = getLatestData(paramPatient.medicaldata, 'sortkey');
+  console.log(
+    'TabViewPatient, deleteMedical, recentData:' + JSON.stringify(recentData),
+  );
+  uri = HOST + '/patients/' + paramPatient._id + '/medical/' + recentData._id;
   // the method value below must be in uppercase
   restOptions = {
     method: 'DELETE',
-    headers: { 'Content-Type': 'application/json' },
-    body: ''
+    headers: {'Content-Type': 'application/json'},
+    body: '',
   };
-  console.log("TabViewPatient, uri: " + uri);
+  console.log('TabViewPatient, uri: ' + uri);
   await fetch(uri, restOptions)
-    .then((response) => response.json())
-    .then((data) => {
-      Alert.alert(
-        "Medical Data",
-        "is deleted successfully!",
-        [
-          { text: "OK", onPress: () => navigation.goBack() }
-        ]
-      );
+    .then(response => response.json())
+    .then(data => {
+      Alert.alert('Medical Data', 'is deleted successfully!', [
+        {text: 'OK', onPress: () => navigation.goBack()},
+      ]);
     })
-    .catch((response) => {
+    .catch(response => {
       // error saveing the data
-      console.log("TabViewPatient, deleteMedical failed!!!!!!");
-      console.log("response: " + response);
-      Alert.alert(
-        "Error Deleting Medical Data",
-        response.toString(),
-        [
-          { text: "OK"}
-        ]
-      );
+      console.log('TabViewPatient, deleteMedical failed!!!!!!');
+      console.log('response: ' + response);
+      Alert.alert('Error Deleting Medical Data', response.toString(), [
+        {text: 'OK'},
+      ]);
+    });
+}
+
+async function deletePatient(paramPatient, navigation) {
+  uri = HOST + '/patients/' + paramPatient._id;
+  // the method value below must be in uppercase
+  restOptions = {
+    method: 'DELETE',
+    headers: {'Content-Type': 'application/json'},
+    body: '',
+  };
+  console.log('TabViewPatient, uri: ' + uri);
+  await fetch(uri, restOptions)
+    .then(response => response.json())
+    .then(data => {
+      Alert.alert('Patient', 'Deleted successfully!', [
+        {text: 'OK', onPress: () => navigation.goBack()},
+      ]);
+    })
+    .catch(response => {
+      // error saveing the data
+      console.log('TabViewPatient, deletePatient failed!!!!!!');
+      console.log('response: ' + response);
+      Alert.alert('Error Deleting Patient', response.toString(), [
+        {text: 'OK'},
+      ]);
     });
 }
 
